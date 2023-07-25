2cdd67eebb0f2b8df41fa2a99bd0bae5bdb06967
Blueturboguy07 <mann.bellani.0426@gmail.com>
2023-07-25 14:20:06 -0500

    Initial Commit

---
+++ CarbonTrackrOfficial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarbonTrackrOfficial
+//
+//  Created by Manish Bellani on 7/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
